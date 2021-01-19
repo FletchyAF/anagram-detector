3f2f95c01a9740e5d892bda3ee5d405ae3bc9327
FletchyAF <joshua.f1998@gmail.com>
2021-01-19 12:58:15 -0500

    commit tree 29551cf3227e19c42bcc84863e61ef269d3eb891

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |member|
+            matches = member.length == @word.length 
+            if matches
+                @word.split("").each do |letter|
+                    if member.count(letter) != @word.count(letter)
+                        matches = false
+                    end
+                end
+            end
+            next matches
+        end
+    end
+end
